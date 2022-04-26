951c33632c03f1fbcc1a699ffd1e30896a3fb18a
DESKTOP-17R30V6\david <nekoder777@gmail.com>
2022-04-26 19:28:03 +0900

    let

---
+++ VanillaJS/let.js
@@ -0,0 +1,12 @@
+// let: 재선언 불가능, 재할당 가능
+// 변수 선언
+let testValue;
+testValue = 1;
+// print
+console.log(testValue);
+// 재할당 가능
+testValue = 2;
+console.log(testValue);
+
+// let으로 선언시, 동일한 변수명으로 재선언 불가
+// let testValue; (Syntax Error)
